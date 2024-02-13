28de2ab83c9087b2c210bc1747999414465b99dc
Dhiru2023 <dhirusingh11@gmail.com>
2024-02-13 13:05:14 +0530

    Update settings.py

--- student/settings.py
+++ student/settings.py
@@ -52,6 +52,18 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.security.SecurityMiddleware',
 )
 
+MIDDLEWARE = (
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.security.SecurityMiddleware',
+)
+
+
 ROOT_URLCONF = 'student.urls'
 
 TEMPLATES = [
